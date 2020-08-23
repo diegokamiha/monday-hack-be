5dfa4cc958f5dfee53e81490a488d7d14be6fae8
Diego Kamiha <diego.kamiha17@gmail.com>
2020-08-23 11:18:22 -0300

    Adding function error handling

--- handler.js
+++ handler.js
@@ -7,23 +7,13 @@ const dialogflow = require('@google-cloud/dialogflow');
 const sessionClient = new dialogflow.SessionsClient();
 
 module.exports.hello = async event => {
-  var req = JSON.parse(event.body);
-  console.log(req)
-
-  var res = await detectIntent(req.text)
-
-  console.log(res)
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(res),
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Credentials": true,
-      "access-control-allow-methods": "GET,POST,OPTIONS,PUT,DELETE"
-    }
-  };
+  try {
+    var req = JSON.parse(event.body);
+    var res = await detectIntent(req.text)
+    return done(200, res)
+  } catch (error) {
+    return done(500, null, null, error)
+  }
 };
 
 async function detectIntent(query) {
@@ -46,4 +36,39 @@ async function detectIntent(query) {
 
   const responses = await sessionClient.detectIntent(request);
   return responses[0];
+}
+
+function done(statusCode, payload, extraHeaders, err) {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Credentials": true,
+    "access-control-allow-methods": "GET,POST,OPTIONS,PUT,DELETE"
+  }
+
+  // Copies extra headers to header object
+  if (extraHeaders) {
+    for (var key in extraHeaders) {
+      headers[key] = extraHeaders[key]
+    }
+  }
+
+  var responseModel = {}
+
+  if (err) {
+    // Default statusCode for error is 500
+    statusCode = err.statusCode ? err.statusCode : 500
+    responseModel.error = {
+      message: err.message,
+      description: err.description
+    }
+  } else if (payload) {
+    responseModel = payload
+  }
+
+  return {
+    statusCode: statusCode,
+    headers: headers,
+    body: JSON.stringify(responseModel)
+  }
 }
